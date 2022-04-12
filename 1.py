5092e64e1af294f3fa7e90d3282f30c6e4eeb16a
Omkar Dorugade <100912340+omkar5252@users.noreply.github.com>
2022-04-12 11:54:17 +0530

    Update and rename 1.py to check if the given number is positive or negative

--- 1.py
+++
@@ -1,7 +0,0 @@
-# Problem 1 - A program to check if the given number is positive or negative.
-
-num = int(input("Enter a number:"))
-if (num > 0):
-    print(num,"is positive number.")
-else:
-    print(num,"is negative number.")
